3a0880e7b9cf90a2d2d285a5d338ea2df9d5575f
brettapitz <53789094+brettapitz@users.noreply.github.com>
2021-09-28 08:57:13 -0400

    Added csc challenge problem

---
+++ csc_problem.js
@@ -0,0 +1,34 @@
+module.exports = function sortCategoriesForInsert (inputJson) {
+    const data = JSON.parse(inputJson);
+    var queue = [];
+    var ret = [];
+    var mp = new Map();
+
+    // Turn input data into a Map keyed to parent_id; elements with null parent_id 
+    // are pushed to a queue for later.
+    // Map values are stored as arrays to allow for multiple elements with the same parent.
+    for (const el of data) {
+        if (el.parent_id == null) {
+            queue.push(el);
+        } else if (mp.has(el.parent_id)) {
+            mp.get(el.parent_id).push(el)
+        } else {
+            mp.set(el.parent_id, [el]);
+        }
+    }
+
+    // Iterate through parent elements in queue, keying into map with their ids
+    // to get their children. Children are added to a new queue, and the old queue 
+    // is moved to ret.
+    while (ret.length != data.length) {
+        var temp = [];
+        for (const el of queue) {
+            if (mp.has(el.id)) {
+                temp.push(...mp.get(el.id));
+            }
+        }
+        ret.push(...queue);
+        queue = temp;
+    }
+    return JSON.stringify(ret);
+}
