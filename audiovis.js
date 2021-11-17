7e5d198cb4605e6a5cc76e62ed288c2cef6f290d
brettapitz <53789094+brettapitz@users.noreply.github.com>
2021-11-17 11:24:05 -0500

    Added spectrogram to audiovis

--- audiovis.js
+++ audiovis.js
@@ -12,7 +12,7 @@ var options = document.getElementsByClassName('vis_option');
 
 source.connect(analyser).connect(audioctx.destination);
 
-analyser.fftSize=16384;
+analyser.fftSize=8192;
 canvasctx.strokeStyle = '#0ff';
 canvasctx.lineWidth = 2;
 const bgColor = 'black';
@@ -24,6 +24,9 @@ const scopeData = new Uint8Array(analyser.fftSize);
 const freqData = new Uint8Array(analyser.frequencyBinCount);
 const freqBarWidth = Math.floor(canvas.width / Math.log2(analyser.fftSize));
 const heightByteRatio = canvas.height / 256;
+const specFillRate = 2;
+const specShadowWidth = 40;
+var specPosition = 0;
 
 function drawScope() {
   analyser.getByteTimeDomainData(scopeData);
@@ -43,12 +46,12 @@ function drawScope() {
   requestAnimationFrame(draw);
 }
 
-function logAverage(arr) {
+function logAverage(arr, base) {
   let ret = [];
   let i = 0;
 
   while (i < arr.length) {
-    let next_i = 2 * i + 1;
+    let next_i = base * i + 1;
     let average = arr.slice(i, next_i).reduce((acc, el) => acc + el) / (i + 1);
     ret.push(average);
     i = next_i;
@@ -57,14 +60,14 @@ function logAverage(arr) {
   return ret;
 }
 
-function drawFreq() {
+function drawHist() {
   analyser.getByteFrequencyData(freqData);
 
   canvasctx.fillStyle = bgColor;
   canvasctx.fillRect(0, 0, canvas.width, canvas.height);
   canvasctx.fillStyle = freqBarColor;
 
-  let bars = logAverage(freqData);
+  let bars = logAverage(freqData, 2);
   let x = 0;
   let y = 0;
   for (let bar of bars) {
@@ -75,10 +78,39 @@ function drawFreq() {
   requestAnimationFrame(draw);
 }
 
+function drawSpec() {
+  analyser.getByteFrequencyData(freqData);
+  let grad = canvasctx.createLinearGradient(specPosition, 0, specPosition, canvas.height);
+
+  for (let i = 0; i < freqData.length; i++) {
+    let bin = freqData[i] * 2.2;
+    let low = Math.min(50, bin);
+    let mid = Math.min(255, Math.max(0, bin - 50));
+    let high = Math.max(0, bin - 305);
+    let color = `rgb(${mid}, ${high}, ${low})`
+    grad.addColorStop(1 - Math.sqrt(i/freqData.length), color);
+  }
+
+  canvasctx.fillStyle = grad;
+  canvasctx.fillRect(specPosition, 0, specFillRate, canvas.height);
+  specPosition += specFillRate;
+
+  let shadow = canvasctx.createLinearGradient(specPosition, 0, specPosition + specShadowWidth, 0);
+  shadow.addColorStop(0, 'rgba(0, 0, 0, 0.06');
+  shadow.addColorStop(1, 'rgba(0, 0, 0, 0');
+  canvasctx.fillStyle = shadow;
+  canvasctx.fillRect(specPosition, 0, specShadowWidth, canvas.height);
+
+  if (specPosition > canvas.width) specPosition = 0;
+
+  requestAnimationFrame(draw);
+}
+
 function draw() {
   switch (visOption.id) {
     case "scope": drawScope(); break;
-    case "freq": drawFreq(); break;
+    case "hist": drawHist(); break;
+    case "spec": drawSpec(); break;
   }
 }
 
@@ -89,5 +121,7 @@ for (let option of options) {
     visOption.classList.remove('active');
     e.target.classList.add('active');
     visOption = e.target;
+    canvasctx.fillStyle = bgColor;
+    canvasctx.fillRect(0, 0, canvas.width, canvas.height);
   })
 }
